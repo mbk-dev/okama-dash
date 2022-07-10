59a26bac811848af6fba20b2e957aa6c865caa30
Sergey Kikevich <sergey@rostsber.ru>
2022-07-10 15:48:15 +0300

    refactor: get_assets_names

--- common/assets_names_dash_table.py
+++ common/assets_names_dash_table.py
@@ -3,18 +3,13 @@ import pandas as pd
 from dash import dash_table
 
 
-def get_assets_names(al_object: okama.AssetList):
+def get_assets_names(al_object: okama.AssetList) -> dash_table.DataTable:
     names_df = (
         pd.DataFrame.from_dict(al_object.names, orient="index")
         .reset_index(drop=False)
         .rename(columns={"index": "Ticker", 0: "Long name"})[["Ticker", "Long name"]]
     )
-    names_table = dash_table.DataTable(
+    return dash_table.DataTable(
         data=names_df.to_dict(orient="records"),
-        style_data={
-            "whiteSpace": "normal",
-            "height": "auto",
-        },
-        page_size=4,
-    )
-    return names_table
+        style_data={"whiteSpace": "normal", "height": "auto",},
+        page_size=4,)
