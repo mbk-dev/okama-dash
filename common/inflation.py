e4040dda8f71426d56c7bfe7c013cfc509d47bc6
Sergey Kikevich <sergey@rostsber.ru>
2022-07-13 15:48:54 +0300

    fix: don't cache the currency list

--- common/inflation.py
+++ common/inflation.py
@@ -1,9 +1,6 @@
-from functools import lru_cache
-
 import okama as ok
 
 
-@lru_cache()
 def get_currency_list():
     inflation_list = ok.symbols_in_namespace("INFL").symbol.tolist()
     return [x.split(".", 1)[0] for x in inflation_list]
