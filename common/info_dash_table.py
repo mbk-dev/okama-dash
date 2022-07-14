29cc428e898698712b36ee4c601f3010b862a217
Sergey Kikevich <sergey@rostsber.ru>
2022-07-14 17:33:12 +0300

    feat: no need in "Shortest history" and "Longest history" if only one ticker in "Compare"

--- common/info_dash_table.py
+++ common/info_dash_table.py
@@ -21,15 +21,18 @@ def get_info(al_object):
     al_object.assets_first_dates.pop(ccy)
     eldest_ticker = list(al_object.assets_first_dates)[0]
     eldest_asset_date = al_object.assets_first_dates[eldest_ticker].strftime("%Y-%m")
-    info_dict = [
+    info_list = [
         {"Property": "First Date", "Value": al_object.first_date.strftime("%Y-%m")},
         {"Property": "Last datee", "Value": al_object.last_date.strftime("%Y-%m")},
         {"Property": "Period length", "Value": al_object._pl_txt},
         {"Property": "Shortest history", "Value": f"{al_object.newest_asset} - {newest_asset_date}"},
         {"Property": "Longest history", "Value": f"{eldest_ticker} - {eldest_asset_date}"}
     ]
+    if len(al_object.symbols) < 2:
+        # no need in "Shortest history" and "Longest history" if only one ticker
+        info_list = info_list[:3]
     info_table = dash_table.DataTable(
-        data=info_dict,
+        data=info_list,
         style_data={"whiteSpace": "normal", "height": "auto"},
     )
     return info_table
