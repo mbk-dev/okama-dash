a743167bda99df7ab8ab4e81720652be4f003b09
Sergey Kikevich <sergey@rostsber.ru>
2022-07-22 08:40:52 +0300

    feat: add XFRA and XTAE symbols

--- common/symbols.py
+++ common/symbols.py
@@ -9,7 +9,7 @@ def get_symbols() -> list:
     """
     Get all available symbols (tickers) from assets namespaces.
     """
-    namespaces = ["US", "LSE", "MOEX", "INDX", "COMM", "FX", "CC"]
+    namespaces = ["US", "LSE", "XFRA", "XTAE", "MOEX", "INDX", "COMM", "FX", "CC"]
     list_of_symbols = [ok.symbols_in_namespace(ns).symbol for ns in namespaces]
     classifier_df = pd.concat(
         list_of_symbols, axis=0, join="outer", copy="false", ignore_index=True
