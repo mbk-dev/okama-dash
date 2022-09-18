ebf780906008b2617bf573db8165223f5b7ce6b8
Sergey Kikevich <sergey@rostsber.ru>
2022-09-18 13:52:52 +0300

    feat: add portfolio page to navigation

---
+++ navigation.py
@@ -0,0 +1,28 @@
+import dash
+import dash_bootstrap_components as dbc
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Efficient Frontier", href="/")),
+        dbc.NavItem(dbc.NavLink("Compare Assets", href="/compare")),
+        dbc.NavItem(dbc.NavLink("Portfolio", href="/portfolio")),
+        dbc.DropdownMenu(
+            children=[
+                # dbc.DropdownMenuItem("More pages", header=True),
+                # dbc.DropdownMenuItem(
+                #     f"{page['name']}, href={page['relative_path']}"
+                # )
+                # for page in dash.page_registry.values()
+                dbc.DropdownMenuItem("Community forums", href="https://community.okama.io"),
+                # dbc.DropdownMenuItem("Compare Assets", href="/")
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+        ),
+    ],
+    brand="Okama widgets",
+    brand_href="/",
+    color="primary",
+    dark=True,
+)
