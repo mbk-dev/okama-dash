67befe73d664915d3fb66ba9e942eba309934cde
Sergey Kikevich <sergey@rostsber.ru>
2022-11-21 13:35:42 +0300

    docs: fix EF widget description

--- pages/efficient_frontier/cards_efficient_frontier/eng/ef_description_txt.py
+++ pages/efficient_frontier/cards_efficient_frontier/eng/ef_description_txt.py
@@ -5,7 +5,7 @@ ef_description_text = dcc.Markdown(
     Efficient Frontier widget uses monthly total return historical data to calculate optimized portfolio points.
     Optimization uses annualized risk (annualized standard deviation) as an utility function. 
     
-    Widget constructor has different types of portfolio rebalancing strategy (not rebalanced, monthly or annually rebalanced).  
+    Widget uses constantly rebalanced portfolios (monthly rebalanced).  
 
     The rebalancing is the action of bringing the portfolio that has deviated away
     from original asset allocation back into line. After rebalancing the portfolio assets
