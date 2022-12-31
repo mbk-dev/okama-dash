eff54759361591a93ed29e88b955700dc3f933ab
Sergey Kikevich <sergey@rostsber.ru>
2022-12-31 18:20:11 +0300

    feat: change flask-cache config to redis

--- common/__init__.py
+++ common/__init__.py
@@ -1,9 +1,15 @@
 from flask_caching import Cache
 
 cache = Cache(
+    # config={
+    #     "CACHE_TYPE": "filesystem",
+    #     "CACHE_DIR": "cache-directory",
+    #     "CACHE_DEFAULT_TIMEOUT": 300,
+    # },
     config={
-        "CACHE_TYPE": "filesystem",
-        "CACHE_DIR": "cache-directory",
-        "CACHE_DEFAULT_TIMEOUT": 300,
+        "CACHE_TYPE": "RedisCache",
+        "CACHE_REDIS_URL": "redis://localhost:6379/0",
+        "CACHE_DEFAULT_TIMEOUT": 500,
     },
+
 )
