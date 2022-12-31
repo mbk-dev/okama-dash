5bd041ecfacd6359eaa3767fb5f707312432e395
Sergey Kikevich <sergey@rostsber.ru>
2022-12-31 13:08:17 +0300

    docs: fix database widget description

--- pages/database/cards_database/eng/db_description_txt.py
+++ pages/database/cards_database/eng/db_description_txt.py
@@ -4,26 +4,26 @@ db_description_text = dcc.Markdown(
     """
     Financial Database of *okama* has stock market securities, currencies, 
     commodities and indexes as well as macroeconomic indicators historical data.   
-    ### Stock markets
+    ##### Stock markets
 
     - Stocks and ETF for main world markets
     - Mutual funds
     - Commodities
     - Stock indexes
     
-    ### Currencies
+    ##### Currencies
     
     - FX currencies
     - Crypto currencies
     - Central bank exchange rates
     
-    ### Macroeconomic indicators
+    ##### Macroeconomic indicators
     
     - Inflation (USA, United Kingdom, European Union, Israel, Russia etc.)
     - Central bank rates
     - GDP (Gross Domestic Product)
     
-    ### Other historical data
+    ##### Other historical data
     
     - Real estate prices
     - Top bank rates
