7bb8e65f7615c72f2e4b9f75d2fc3bcd0ade9029
Sergey Kikevich <sergey@rostsber.ru>
2024-06-18 17:13:16 +0300

    chore: decrease the number of allowed tickers from 12 to 15

--- common/settings.py
+++ common/settings.py
@@ -9,4 +9,4 @@ namespaces = okama.assets_namespaces
 MONTHS_PER_YEAR = 12
 MC_EF_MAX = 5000  # Max points in Monte-Carlo simulation in Efficient Frontier
 MC_PORTFOLIO_MAX = 50  # Max wealth time series in Monte-Carlo simulation in Portfolio
-ALLOWED_NUMBER_OF_TICKERS = 15  # max number of tickers in portfolio, EF or AssetList
+ALLOWED_NUMBER_OF_TICKERS = 12  # max number of tickers in portfolio, EF or AssetList
