f8951f515ac96bce749b95463d5857850dd10b08
Sergey Kikevich <sergey@rostsber.ru>
2024-07-17 13:43:14 +0300

    feat: add 75th percentile to Portoflio Monte-Carlo statistics

--- pages/portfolio/portfolio.py
+++ pages/portfolio/portfolio.py
@@ -315,6 +315,7 @@ def get_forecast_survival_statistics_table(df_forecast, df_backtsest, pf_object:
             {"1": "5th percentile", "2": fsp.quantile(5 / 100), "3": "Max", "4": fsp.max()},
             {"1": "25th percentile", "2": fsp.quantile(25 / 100), "3": "Mean", "4": fsp.mean()},
             {"1": "50th percentile", "2": fsp.quantile(50 / 100), "3": "Mean", "4": fsp.std()},
+            {"1": "75th percentile", "2": fsp.quantile(75 / 100), "3": "-", "4": None},
             {"1": "99th percentile", "2": fsp.quantile(99 / 100), "3": "-", "4": None},
         ]
         columns = [
@@ -353,6 +354,7 @@ def get_forecast_wealth_statistics_table(df_forecast) -> dash_table.DataTable:
             {"1": "5th percentile", "2": wealth.quantile(5 / 100), "3": "Max", "4": wealth.max()},
             {"1": "25th percentile", "2": wealth.quantile(25 / 100), "3": "Mean", "4": wealth.mean()},
             {"1": "50th percentile", "2": wealth.quantile(50 / 100), "3": "Std", "4": wealth.std()},
+            {"1": "75th percentile", "2": wealth.quantile(25 / 100), "3": "-", "4": None},
             {"1": "99th percentile", "2": wealth.quantile(99 / 100), "3": "-", "4": None},
         ]
         columns = [
